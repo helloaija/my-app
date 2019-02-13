fea9672967da386f5a2db422ba72e13ba472e6a1
helloaija <shitongle163@126.com>
2019-02-13 22:10:05 +0800

    菜单

--- src/app/sys/menuManage/nenu-manage.component.ts
+++ src/app/sys/menuManage/nenu-manage.component.ts
@@ -1,15 +1,68 @@
 import {Component, OnInit} from '@angular/core';
+import {NzFormatEmitEvent, NzMessageService} from 'ng-zorro-antd';
+import {FormBuilder, Validators} from '@angular/forms';
+import {CommonUtils} from '../../common/CommonUtils';
 
 @Component({
-  selector: 'view-menu-manage',
-  template: `
-    xxuserManagePagefafagahahajhsjsjshshws
-  `
+    selector: 'view-menu-manage',
+    templateUrl: './menu-manage.component.html',
+    styleUrls: ['./menu-manage.component.less']
 })
 
 export class MenuManageComponent implements OnInit {
 
-  ngOnInit(): void {
-    alert("menu");
-  }
+    nodes: any;
+    editForm: any;
+    isSpinning = false;
+
+    constructor(private fb: FormBuilder,
+                private commonUtils: CommonUtils, private messageBar: NzMessageService) {
+    }
+
+    nodeClick(event: NzFormatEmitEvent): void {
+        console.log(event);
+    }
+
+    ngOnInit(): void {
+        this.editForm = this.fb.group({
+            email: [null, [Validators.email, Validators.required]],
+            password: [null, [Validators.required]],
+            checkPassword: [null, [Validators.required]],
+            nickname: [null, [Validators.required]],
+            phoneNumberPrefix: ['+86'],
+            phoneNumber: [null, [Validators.required]],
+            website: [null, [Validators.required]],
+            captcha: [null, [Validators.required]],
+            agree: [false]
+        });
+
+        this.nodes = [{
+            title: 'parent 1',
+            key: '100',
+            expanded: true,
+            children: [{
+                title: 'parent 1-0',
+                key: '1001',
+                expanded: true,
+                children: [
+                    {title: 'leaf', key: '10010', isLeaf: true},
+                    {title: 'leaf', key: '10011', isLeaf: true},
+                    {title: 'leaf', key: '10012', isLeaf: true}
+                ]
+            }, {
+                title: 'parent 1-1',
+                key: '1002',
+                children: [
+                    {title: 'leaf', key: '10020', isLeaf: true}
+                ]
+            }, {
+                title: 'parent 1-2',
+                key: '1003',
+                children: [
+                    {title: 'leaf', key: '10030', isLeaf: true},
+                    {title: 'leaf', key: '10031', isLeaf: true}
+                ]
+            }]
+        }];
+    }
 }
--- src/app/sys/menuManage/nenu-manage.module.ts
+++ src/app/sys/menuManage/nenu-manage.module.ts
@@ -1,22 +1,23 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import {MenuManageComponent} from "./nenu-manage.component";
-import {MenuManageService} from "./nenu-manage.service";
-import {MenuManageRoutingModule} from "./nenu-manage-routing.module";
+import {MenuManageComponent} from './nenu-manage.component';
+import {MenuManageService} from './nenu-manage.service';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    MenuManageRoutingModule
-  ],
-  declarations: [
-    MenuManageComponent
-  ],
-  exports: [MenuManageComponent],
-  providers: [MenuManageService]
+    imports: [
+        CommonModule,
+        FormsModule,
+        NgZorroAntdModule,
+        ReactiveFormsModule
+    ],
+    declarations: [
+        MenuManageComponent
+    ],
+    exports: [MenuManageComponent],
+    providers: [MenuManageService]
 })
 export class MenuManageModule {
 }
