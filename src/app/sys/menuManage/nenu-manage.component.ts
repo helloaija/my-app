2e4fdd965b02857b94772903e26b4fe60ef2cd69
helloaija <shitongle163@126.com>
2019-02-14 18:31:46 +0800

    菜单管理

--- src/app/sys/menuManage/nenu-manage.component.ts
+++ src/app/sys/menuManage/nenu-manage.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {NzFormatEmitEvent, NzMessageService} from 'ng-zorro-antd';
-import {FormBuilder, Validators} from '@angular/forms';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NzFormatEmitEvent, NzMessageService, NzTreeComponent} from 'ng-zorro-antd';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CommonUtils} from '../../common/CommonUtils';
+import {MenuManageService} from "./nenu-manage.service";
+import {finalize} from "rxjs/operators";
 
 @Component({
     selector: 'view-menu-manage',
@@ -10,59 +12,214 @@ import {CommonUtils} from '../../common/CommonUtils';
 })
 
 export class MenuManageComponent implements OnInit {
-
+    // 树节点数据
     nodes: any;
-    editForm: any;
+    editForm: FormGroup;
     isSpinning = false;
+    isEditFormShow = false;
+    isConfirmVisible = false;
 
-    constructor(private fb: FormBuilder,
+
+    // 当前编辑的菜单父ID
+    parentId: any;
+    // 当前编辑节点ID，新增则为空
+    menuId: any;
+    // 删除的节点title
+    deleteNodeTitle: string;
+    // 编辑表单标题
+    editFormTitle: string;
+
+    @ViewChild('menuTree')
+    menuTree: NzTreeComponent;
+
+    constructor(private fb: FormBuilder, private menuService: MenuManageService,
                 private commonUtils: CommonUtils, private messageBar: NzMessageService) {
     }
 
-    nodeClick(event: NzFormatEmitEvent): void {
-        console.log(event);
-    }
-
     ngOnInit(): void {
-        this.editForm = this.fb.group({
-            email: [null, [Validators.email, Validators.required]],
-            password: [null, [Validators.required]],
-            checkPassword: [null, [Validators.required]],
-            nickname: [null, [Validators.required]],
-            phoneNumberPrefix: ['+86'],
-            phoneNumber: [null, [Validators.required]],
-            website: [null, [Validators.required]],
-            captcha: [null, [Validators.required]],
-            agree: [false]
+        this.isSpinning = true;
+        this.menuService.getMenuTree().pipe(finalize(() => {
+            this.isSpinning = false;
+        })).subscribe(data => {
+            if ('0000' == data['resultCode']) {
+                this.nodes = data['result'];
+            } else {
+                this.messageBar.create("error", data['resultMessage']);
+            }
         });
 
-        this.nodes = [{
-            title: 'parent 1',
-            key: '100',
-            expanded: true,
-            children: [{
-                title: 'parent 1-0',
-                key: '1001',
-                expanded: true,
-                children: [
-                    {title: 'leaf', key: '10010', isLeaf: true},
-                    {title: 'leaf', key: '10011', isLeaf: true},
-                    {title: 'leaf', key: '10012', isLeaf: true}
-                ]
-            }, {
-                title: 'parent 1-1',
-                key: '1002',
-                children: [
-                    {title: 'leaf', key: '10020', isLeaf: true}
-                ]
-            }, {
-                title: 'parent 1-2',
-                key: '1003',
-                children: [
-                    {title: 'leaf', key: '10030', isLeaf: true},
-                    {title: 'leaf', key: '10031', isLeaf: true}
-                ]
-            }]
-        }];
+        this.editForm = this.fb.group({
+            name: ['', [Validators.required]],
+            menuCode: ['', [Validators.required]],
+            menuType: ['MENU', [Validators.required]],
+            url: ['', [Validators.required]],
+            status: ['enable', [Validators.required]],
+            remark: ['', []]
+        });
+    }
+
+    /**
+     * 点击节点
+     * @param event
+     */
+    nodeClick(event: NzFormatEmitEvent): void {
+        // 根目录
+        if (null == event.node.key) {
+            this.isEditFormShow = false;
+            return;
+        }
+
+        this.editFormTitle = `编辑节点[${event.node.title}]`;
+        this.isEditFormShow = true;
+
+        this.isSpinning = true;
+        this.menuService.getMenu(event.node.key).pipe(finalize(() => {
+            this.isSpinning = false;
+        })).subscribe(data => {
+            if ('0000' == data['resultCode']) {
+                this.parentId = data['result']['parentId'];
+                this.menuId = data['result']['id'];
+
+                this.editForm.patchValue(data['result']);
+            } else {
+                this.messageBar.create('error', data['resultMessage'])
+            }
+        });
+    }
+
+    /**
+     * 添加子节点
+     */
+    addNodeClick(): void {
+        let selectedNodeList = this.menuTree.getSelectedNodeList();
+        if (selectedNodeList == null || selectedNodeList.length == 0) {
+            this.messageBar.create('warning', '请选择父节点');
+            return;
+        }
+
+        this.editFormTitle = `添加子节点，父节点[${selectedNodeList[0]['title']}]`;
+        this.isEditFormShow = true;
+        this.resetForm();
+        this.parentId = selectedNodeList[0]['key'];
+        this.menuId = '';
+    }
+
+    /**
+     * 删除节点
+     */
+    delNodeClick(): void {
+        let selectedNodeList = this.menuTree.getSelectedNodeList();
+        if (selectedNodeList == null || selectedNodeList.length == 0) {
+            this.messageBar.create('warning', '请选择需要删除的节点！');
+            return;
+        }
+
+        if (selectedNodeList[0].key == '' || selectedNodeList[0].key == '0' || selectedNodeList[0].key == null) {
+            this.messageBar.create('warning', '不能删除根节点节点！');
+            return;
+        }
+
+        this.isConfirmVisible = true;
+        this.deleteNodeTitle = selectedNodeList[0].title;
+    }
+
+    /**
+     * 提交数据
+     */
+    submitForm(): void {
+        for (const i in this.editForm.controls) {
+            this.editForm.controls[i].markAsDirty();
+            this.editForm.controls[i].updateValueAndValidity();
+        }
+
+        if (this.editForm.invalid) {
+            return;
+        }
+
+        if (null == this.menuId || '' == this.menuId) {
+            this.isSpinning = true;
+            this.menuService.addMenu(this.getSaveData()).pipe(finalize(() => {
+                this.isSpinning = false;
+            })).subscribe(data => {
+                if ('0000' == data['resultCode']) {
+                    let addNodes = [];
+                    addNodes.push(data['result']);
+                    this.menuTree.getSelectedNodeList()[0].addChildren(addNodes);
+                    this.isEditFormShow = false;
+                    this.messageBar.create('info', '添加节点成功！');
+                } else {
+                    this.messageBar.create('error', data['resultMessage']);
+                }
+            });
+        } else {
+            this.isSpinning = true;
+            this.menuService.updateMenu(this.getSaveData()).pipe(finalize(() => {
+                this.isSpinning = false;
+            })).subscribe(data => {
+                if ('0000' == data['resultCode']) {
+                    this.menuTree.getSelectedNodeList()[0].title = this.getSaveData()['name'];
+                    this.messageBar.create('info', '更新节点成功！');
+                } else {
+                    this.messageBar.create('error', data['resultMessage']);
+                }
+            });
+        }
+    }
+
+    /**
+     * 重置表单
+     */
+    resetForm(): void {
+        this.editForm.reset({menuType: 'MENU', status: 'enable'});
+    }
+
+    /**
+     * 获取保存数据
+     */
+    getSaveData(): object {
+        let params = this.editForm.getRawValue();
+        params['id'] = this.menuId;
+        params['parentId'] = this.parentId;
+        return params;
+    }
+
+    /**
+     * 确认对话框-取消
+     */
+    confirmHandleCancel(): void {
+        this.isConfirmVisible = false;
+    }
+
+    /**
+     * 确认对话框-确认
+     */
+    confirmHandleOk(): void {
+        this.isConfirmVisible = false;
+
+        let selectedNode = this.menuTree.getSelectedNodeList()[0];
+
+        this.isSpinning = true;
+        this.menuService.deleteMenu(selectedNode['key']).pipe(finalize(() => {
+            this.isSpinning = false;
+        })).subscribe(data => {
+            if ('0000' == data['resultCode']) {
+                this.isEditFormShow = false;
+
+                let childrens = [];
+                let parentNode = selectedNode.getParentNode();
+                parentNode.getChildren().forEach(function (node, index) {
+                    if (node != selectedNode) {
+                        childrens.push(node);
+                    }
+                });
+
+                parentNode.clearChildren();
+                parentNode.addChildren(childrens);
+
+                this.messageBar.create('info', '删除节点成功！');
+            } else {
+                this.messageBar.create('error', data['resultMessage']);
+            }
+        });
     }
 }
--- src/app/sys/menuManage/nenu-manage.service.ts
+++ src/app/sys/menuManage/nenu-manage.service.ts
@@ -1,6 +1,67 @@
 import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable()
 export class MenuManageService {
+    queryMenuTreeUrl = "/admin/menu/queryMenuTree";
+    addMenuUrl = '/admin/menu/addMenu';
+    deleteMenuUrl = '/admin/menu/deleteMenu';
+    getMenuUrl = '/admin/menu/getMenuById';
+    updateMenuUrl = '/admin/menu/updateMenu';
+
+    /**
+     * 获取菜单树
+     */
+    getMenuTree(): Observable<Object> {
+        return this.http.get(`${this.queryMenuTreeUrl}`, {
+            headers: {'X-Requested-With': 'XMLHttpRequest'}
+        });
+    }
+
+    /**
+     * 添加节点
+     * @param params
+     */
+    addMenu(params): Observable<Object> {
+        return this.http.post(`${this.addMenuUrl}`, null, {
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            params: params
+        });
+    }
+
+    /**
+     * 删除节点
+     * @param menuId
+     */
+    deleteMenu(menuId): Observable<Object> {
+        return this.http.post(`${this.deleteMenuUrl}`, null,
+            {headers: {'X-Requested-With': 'XMLHttpRequest'}, params: {menuId: menuId}});
+    }
+
+    /**
+     * 获取节点数据
+     * @param menuId
+     */
+    getMenu(menuId): Observable<Object> {
+        return this.http.get(`${this.getMenuUrl}`, {
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            params: {menuId: menuId}
+        });
+    }
+
+    /**
+     * 更新节点
+     * @param params
+     */
+    updateMenu(params): Observable<Object> {
+        return this.http.post(`${this.updateMenuUrl}`, null, {
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            params: params
+        });
+    }
+
+    constructor(private http: HttpClient) {
+    }
 }
