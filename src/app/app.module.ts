20bc92dc40e6b2fb91f3514f86fa0b30dbf253a5
helloaija <shitongle163@126.com>
2019-02-19 22:55:20 +0800

    动态加载菜单

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {PageNotFoundComponent} from "./common/404/page-not-found.component";
-import {LoginModule} from "./login/login.module";
-import {ContentModule} from "./content/content.module";
+import {PageNotFoundComponent} from './common/404/page-not-found.component';
+import {LoginModule} from './login/login.module';
+import {ContentModule} from './content/content.module';
 import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import {AuthInterceptor} from "./AuthInterceptor";
-import {CommonUtils} from "./common/CommonUtils";
+import {AuthInterceptor} from './AuthInterceptor';
+import {CommonUtils} from './common/CommonUtils';
 
 
 registerLocaleData(zh);
